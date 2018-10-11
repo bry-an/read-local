0a76d7041ca59b4d4b717ab7131666b6815adc41
MBrennecke <MBrennecke10@gmail.com>
2018-10-11 12:15:45 -0600

    finally a map

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import API from "../../utils/API";
-import  { MapTemp } from  "../../components/MapTemp";
+import Map from  "../../components/Map";
 import { Col, Row, Container } from "../../components/Grid";
 
 
@@ -10,7 +10,7 @@ class Home extends Component {
       <Container>
         <Row>
           <Col size="twelve columns">
-            <MapTemp/>
+            <Map/>
           </Col>
         </Row>
       </Container>
