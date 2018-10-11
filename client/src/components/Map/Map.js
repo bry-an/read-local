d74fda25396ed1e48657c838b947b658df7738be
Matthew Brennecke <mbrennecke10@gmail.com>
2018-10-10 20:38:44 -0600

    why is the map always broken

--- client/src/components/Map/Map.js
+++ client/src/components/Map/Map.js
@@ -22,7 +22,7 @@ const drawMap = () => {
     var path = d3.geo.path()
         .projection(projection);
 
-    var svg = d3.select("body").append("svg")
+    var svg = d3.select("#map").append("svg")
         .attr("width", width)
         .attr("height", height);
 
