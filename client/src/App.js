183745664899f83daca5860d597adf6a04058b33
Bryan Yunis <bbyunis@gmail.com>
2018-10-11 19:24:27 -0600

    google map inserted

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import  Map from  "./components/Map";
+import Map from "./components/Map";
 import Home from "./pages/Home";
 import Footer from "./components/Footer";
 import Navbar from "./components/Nav/Navbar";
@@ -8,10 +8,10 @@ import Navbar from "./components/Nav/Navbar";
 const App = () => (
   <Router>
     <div>
-		<Route path="/" component={Navbar} />
-    <Route exact path="/" component={Home} />
-	  <Footer>
-    &copy;2018 GABY
+      <Route path="/" component={Navbar} />
+      <Route exact path="/" component={Home} />
+      <Footer>
+        &copy;2018 GABY
 	  </Footer>
     </div>
   </Router>
