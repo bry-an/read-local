c2521599b3fc1d39fdeeed908761b56318dbd0d5
Matthew Brennecke <mbrennecke10@gmail.com>
2018-10-13 12:00:25 -0600

    starting article page

--- client/src/pages/Articles/Articles.js
+++ client/src/pages/Articles/Articles.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Col, Row, Container } from "../../components/Grid";
-import Dashboard from "../../components/Dashboard"
+import { List, ListItem } from "../../components/List";
+import API from "../../utils/API";
 
 
 class Articles extends Component {
@@ -9,19 +10,32 @@ class Articles extends Component {
   };
 
   componentDidMount() {
-
+    API.getArticles()
+      .then(res =>  {this.setState({articles: res.data.articles});}
+	)
+      .catch(err => console.log(err));
   }
 
   render() {
     return (
       <Container>
         <Row>
-          <Col size="four columns">
-		  Achoo
-		  <Dashboard/>
-          </Col>
-          <Col size="eight columns">
-          Achoo!
+          <Col size="twelve columns">
+			{this.state.articles.length ? (
+				  <List>
+					{this.state.articles.map((article, i) => {
+					  return (
+						<ListItem key={article.url}>
+						  <a href={"/articles/" + article.url}>
+							<strong>
+							  {article.title} by {article.author}
+							</strong>
+						  </a>
+						</ListItem>
+					  );
+					})}
+				  </List>
+				) : ""}
           </Col>
         </Row>
       </Container>
