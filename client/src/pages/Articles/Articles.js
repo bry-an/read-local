52024604e7bd55075dac078c79e450d839c0257b
Bryan Yunis <bbyunis@gmail.com>
2018-10-14 13:18:27 -0600

    accepted changes

--- client/src/pages/Articles/Articles.js
+++ client/src/pages/Articles/Articles.js
@@ -1,12 +1,8 @@
 import React, { Component } from "react";
 import { Col, Row, Container } from "../../components/Grid";
-<<<<<<< HEAD
-// import Dashboard from "../../components/Dashboard"
-=======
 import { List, ListItem } from "../../components/List";
 import API from "../../utils/API";
 import "./Articles.css";
->>>>>>> c8324f2a11bd9dcb512214d0c99e5c8357ff179a
 
 
 class Articles extends Component {
@@ -16,8 +12,8 @@ class Articles extends Component {
 
   componentDidMount() {
     API.fillArticles()
-      .then(res =>  {this.setState({articles: res.data});}
-	)
+      .then(res => { this.setState({ articles: res.data }); }
+      )
       .catch(err => console.log(err));
   }
 
@@ -26,22 +22,22 @@ class Articles extends Component {
       <Container>
         <Row>
           <Col size="twelve columns">
-			{this.state.articles.length ? (
-				  <List>
-					{this.state.articles.map((article, i) => {
-						const url = article.url.split("/").pop().split(".").shift();
-					  return (
-						<ListItem key={url} >
-						  <a href={"/articles/" + url} id={"id-" + url}>
-							<strong>
-							  {article.title} <span className="author">by {article.author}</span>
-							</strong>
-						  </a>
-						</ListItem>
-					  );
-					})}
-				  </List>
-				) : ""}
+            {this.state.articles.length ? (
+              <List>
+                {this.state.articles.map((article, i) => {
+                  const url = article.url.split("/").pop().split(".").shift();
+                  return (
+                    <ListItem key={url} >
+                      <a href={"/articles/" + url} id={"id-" + url}>
+                        <strong>
+                          {article.title} <span className="author">by {article.author}</span>
+                        </strong>
+                      </a>
+                    </ListItem>
+                  );
+                })}
+              </List>
+            ) : ""}
           </Col>
         </Row>
       </Container>
